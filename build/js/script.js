1a65b17919e6a7964f26df509c08c698d3a1e6d6
Александр <psychosocial555666@yandex.ru>
2021-05-23 19:40:40 +0300

    Выполнена верстка всех элементов

--- build/js/script.js
+++ build/js/script.js
@@ -29,6 +29,14 @@ const swiper3 = new Swiper('.slider__big', {
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' +
+      '<span class="circle">' +
+        '<span class="circle__half circle__half_left"></span>' +
+        '<span class="circle__half circle__half_right"></span>' +
+      '</span>' +
+    '</span>';
+    },
   },
   controller: {
     control: swiper2,
@@ -36,4 +44,34 @@ const swiper3 = new Swiper('.slider__big', {
   allowTouchMove: false,
 });
 
+let body = document.querySelector('body');
+let menu = body.querySelector('.main-menu');
+let menuHeight = menu.offsetHeight;
 
+let searchButton = menu.querySelector('.user-menu__search');
+let toggleButton = menu.querySelector('.contacts__toggler');
+
+
+toggleButton.addEventListener('click', () => {
+  body.classList.toggle('scroll-block');
+  menu.classList.toggle('main-menu--burger-opened');
+});
+
+searchButton.addEventListener('click', () => {
+  body.classList.toggle('scroll-block');
+  menu.classList.toggle('main-menu--searching');
+});
+
+window.addEventListener('scroll', function() {
+
+  let feedback = body.querySelector('.feedback');
+  if(pageYOffset >= menuHeight) {
+    menu.classList.add('main-menu--fixed');
+    menu.classList.add('main-menu--fadein');
+    feedback.style.marginBottom = menuHeight + 'px';
+  } else {
+    menu.classList.remove('main-menu--fixed');
+    menu.classList.remove('main-menu--fadein');
+    feedback.style.marginBottom = '0px';
+  };
+});
